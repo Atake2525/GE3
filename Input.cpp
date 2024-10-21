40ba0ac28df5ed3893344e689317194b9a46e58a
Atake <146906117+Atake2525@users.noreply.github.com>
2024-10-21 15:06:57 +0900

    確認課題(完了)

--- Input.cpp
+++ Input.cpp
@@ -1,12 +1,25 @@
 #include "Input.h"
 #include "cassert"
+#include <wrl.h>
+#define DIRECTINPUT_VERSION 0x0800
+#include <dinput.h>
+#pragma comment(lib, "dinput8.lib")
+#pragma comment(lib, "dxguid.lib")
 
-void Input::Initialize() {
-	// DirectInputの初期化
-	IDirectInput8* directInput = nullptr;
-	WNDCLASS wc{};
-	HRESULT hr = DirectInput8Create(wc.hInstance, DIRECTINPUT_VERSION, IID_IDirectInput8, (void**)&directInput, nullptr);
-	assert(SUCCEEDED(hr));
+using namespace Microsoft::WRL;
+
+void Input::Initialize(HINSTANCE hInstance, HWND hwnd) {
 	// DirectInputのインスタンス生成
 	ComPtr<IDirectInput8> directInput = nullptr;
+	HRESULT hr = DirectInput8Create(hInstance, DIRECTINPUT_VERSION, IID_IDirectInput8, (void**)&directInput, nullptr);
+	assert(SUCCEEDED(hr));
+	// キーボードデバイス生成
+	hr = directInput->CreateDevice(GUID_SysKeyboard, &keyboard, NULL);
+	assert(SUCCEEDED(hr));
+	// 入力データー形式のセット
+	hr = keyboard->SetDataFormat(&c_dfDIKeyboard);
+	assert(SUCCEEDED(hr));
+	// 排他的制御レベルのセット
+	hr = keyboard->SetCooperativeLevel(hwnd, DISCL_FOREGROUND | DISCL_NONEXCLUSIVE | DISCL_NOWINKEY);
+
 }
